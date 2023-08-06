72a86933738a5883bde2541e08bb226c8a049340
Lautaro Linquimán <lylinquiman@gmail.com>
2023-08-05 23:08:33 -0300

    Se agrega mensaje de error en caso de no agregar JWT_ENDPOINT

--- jwt_client_auth/__init__.py
+++ jwt_client_auth/__init__.py
@@ -2,7 +2,11 @@ __version__ = '0.1.0'
 
 from django.conf import settings
 
-JWT_ENDPOINT = getattr(settings, 'JWT_ENDPOINT')
+try:
+    JWT_ENDPOINT = getattr(settings, 'JWT_ENDPOINT')
+except AttributeError:
+    raise Exception('JWT_ENDPOINT must be defined in settings.py')
+
 JWT_REDIRECT_URL = getattr(settings, 'JWT_REDIRECT_URL', 'login_ok')
 JWT_ENDPOINT_SSL_VERIFY = getattr(settings, 'JWT_ENDPOINT_SSL_VERIFY', True)
 JWT_ENDPOINT_ERROR_MESSAGE_FIELD = getattr(settings, 'JWT_ENDPOINT_ERROR_MESSAGE', 'message')

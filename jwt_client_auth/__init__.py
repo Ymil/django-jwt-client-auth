e9e78b5e0008410f18b1fab60c7f3ce8a435cb98
Lautaro Linquimán <lylinquiman@gmail.com>
2023-08-09 22:04:32 -0300

    Feature: se agrega casteo a boolean para JWT_ENDPOINT_SSL_VERIFY

--- jwt_client_auth/__init__.py
+++ jwt_client_auth/__init__.py
@@ -8,5 +8,5 @@ except AttributeError:
     raise Exception('JWT_ENDPOINT must be defined in settings.py')
 
 JWT_REDIRECT_URL = getattr(settings, 'JWT_REDIRECT_URL', 'login_ok')
-JWT_ENDPOINT_SSL_VERIFY = getattr(settings, 'JWT_ENDPOINT_SSL_VERIFY', True)
+JWT_ENDPOINT_SSL_VERIFY = bool(getattr(settings, 'JWT_ENDPOINT_SSL_VERIFY', True))
 JWT_ENDPOINT_ERROR_MESSAGE_FIELD = getattr(settings, 'JWT_ENDPOINT_ERROR_MESSAGE', 'message')
